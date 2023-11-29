55a9bbbf7517544484914c5828c913e38c8f77f8
GitGetAc <Git.Zoltan.Ladanyi@gmx.com>
2023-11-29 09:18:58 +0100

    Add project files.

---
+++ Pokemon.cs
@@ -0,0 +1,38 @@
+﻿/* With Primary constructor (The whole class is in a constructor)
+
+namespace PokemonKlasseMedConstructor2
+{
+    internal class Pokemon(string name, int health, int level)
+    {
+        public string? Name { get; set; } = name;
+        public int Health { get; set; } = health;
+        public int Level { get; set; } = level;
+
+        public void ShowPokemon()
+        {
+            Console.WriteLine($"{Name} pokemon har {Health}hp og er level: {Level}");
+        }
+    }
+}*/
+
+namespace PokemonKlasseMedConstructor2
+{
+    internal class Pokemon
+    {
+        public string? Name { get; set; }
+        public int Health { get; set; }
+        public int Level { get; set; }
+
+        public Pokemon(string name, int health, int level)
+        {
+            Name = name;
+            Health = health;
+            Level = level;
+        }
+
+        public void ShowPokemon()
+        {
+            Console.WriteLine($"{Name} pokemon har {Health}hp og er level: {Level}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PokemonKlasseMedConstructor2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var Pikachu = new Pokemon ( "Pikachu", 20, 21 );
+            var Belzebub = new Pokemon ( "Belzebub", 11, 66 );
+            var Guaram = new Pokemon ( "Guaram", 44, 50 );
+            
+            Pikachu.ShowPokemon();  //Eller hvis vi kaller egenskapene fra main() inn i en funksjon
+
+            Console.WriteLine($"{Pikachu.Name} pokemon har {Pikachu.Health}hp og er level: {Pikachu.Level}");
+
+            Belzebub.ShowPokemon(); //Eller hvis vi kaller egenskapene fra main() inn i en funksjon
+
+            Console.WriteLine($"{Belzebub.Name} pokemon har {Belzebub.Health}hp og er level: {Belzebub.Level}");
+
+            Guaram.ShowPokemon();   //Eller hvis vi kaller egenskapene fra main() inn i en funksjon
+
+            Console.WriteLine($"{Guaram.Name} pokemon har {Guaram.Health}hp og er level: {Guaram.Level}");
+
+        }
+    }
+}
